fb9475da8b184714e927db269c7b1ed0ca94c0b5
Piotr Daniel <p.daniel.london@gmail.com>
2020-11-02 11:53:40 +0000

    minor fix

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var inputText = document.getElementById("messageInput");
 var decipheredText = document.getElementById("result");
 
 // alphabet array
-const alphabet = [" ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ".", ",", "?", "!", "-", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "$", "%", "&", "(", ")", "/"];
+const alphabet = [" ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ".", ",", "?", "!", "-", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "$", "%", "&", "(", ")", "/", "'"];
 
 // key in numerical value
 var key = 0;
